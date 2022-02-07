b232fff158a45e7633843998d515b01b9ebfb440
Guillermo <guillermo.garcia.grao@gmail.com>
2022-02-07 15:55:16 +0100

    ESLint configured

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        // "plugin:@typescript-eslint/recommended",
+	"plugin:@typescript-eslint/recommended-requiring-type-checking"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+	"project": "tsconfig.eslint.json"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    },
+    "overrides": [
+      {
+        "files": ["tests/**/*.ts"],
+	"env": {"jest": true, "node": true}
+      }
+    ]
+}

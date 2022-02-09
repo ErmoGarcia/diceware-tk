b6d4923423c10c5bc1b97026837c551677ec67b5
Guillermo <guillermo.garcia.grao@gmail.com>
2022-02-09 17:28:45 +0100

    getPassphrase implemented

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
+import { throwNDices } from "./utils/dice"
+import { baseXToDecimal, readWordlistFromFile, readWordlistFromNetwork } from "./utils/wordlist"
 
+export const getRandomWord = async (dices: number[], wordlist: string, localfile: boolean): Promise<string> => {
+    const index = baseXToDecimal(parseInt(dices.join()))
 
+    if(localfile) {
+        const list = await readWordlistFromFile(wordlist)
+        return list[index]
+    }
 
-export const getPassphrase = (nWords: number = 5): string[] => {
-    return Array(nWords).fill("word")
+    else {
+        const list = await readWordlistFromNetwork(wordlist)
+        return list[index]
+    }
+}
+
+export const getPassphrase = async (nWords = 5, nDices = 5, wordlist = 'https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt', localfile = false): Promise<string[]> => {
+    const dices = await throwNDices(nDices)
+    return await Promise.all(Array(nWords).fill(getRandomWord(dices, wordlist, localfile)))
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,13 +4,13 @@ import { getPassphrase } from '../src/index';
 describe.each(['src/dictionaries/DW-Espanol-1.txt', 'src/dictionaries/DW-Espanol-2.txt'])('Playing Diceware', (filename) => {
 
     it('Creating a 5 words passphrase (default)', async () => {
-        const passphrase = getPassphrase()
+        const passphrase = await getPassphrase()
 
         expect(passphrase).toHaveLength(5)
     })
 
     it('Creating a 6 words passphrase', async () => {
-        const passphrase = getPassphrase(6)
+        const passphrase = await getPassphrase(6)
 
         expect(passphrase).toHaveLength(6)
     })
